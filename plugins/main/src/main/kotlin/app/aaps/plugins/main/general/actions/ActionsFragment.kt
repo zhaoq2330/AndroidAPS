zhaoq2330/AndroidAPS

commit 2e85d3be06e437d788d53b2c36aa72ffbf1d349e

RunningMode: version that compile

plugins/main/src/main/kotlin/app/aaps/plugins/main/general/actions/ActionsFragment.kt
@@ -235,10 +235,9 @@ class ActionsFragment : DaggerFragment() {
             activePlugin.activeProfileSource.profile != null &&
                 pump.pumpDescription.isSetBasalProfileCapable &&
                 pump.isInitialized() &&
-                !pump.isSuspended() &&
-                !loop.isDisconnected).toVisibility()
+                loop.runningMode.isLoopRunning()).toVisibility()
 
-        if (!pump.pumpDescription.isExtendedBolusCapable || !pump.isInitialized() || pump.isSuspended() || loop.isDisconnected || pump.isFakingTempsByExtendedBoluses || config.AAPSCLIENT) {
+        if (!pump.pumpDescription.isExtendedBolusCapable || !pump.isInitialized()  || !loop.runningMode.isLoopRunning() || pump.isFakingTempsByExtendedBoluses || config.AAPSCLIENT) {
             binding.extendedBolus.visibility = View.GONE
             binding.extendedBolusCancel.visibility = View.GONE
         } else {
@@ -254,7 +253,7 @@ class ActionsFragment : DaggerFragment() {
             }
         }
 
-        if (!pump.pumpDescription.isTempBasalCapable || !pump.isInitialized() || pump.isSuspended() || loop.isDisconnected || config.AAPSCLIENT) {
+        if (!pump.pumpDescription.isTempBasalCapable || !pump.isInitialized() || !loop.runningMode.isLoopRunning() || config.AAPSCLIENT) {
             binding.setTempBasal.visibility = View.GONE
             binding.cancelTempBasal.visibility = View.GONE
         } else {
@@ -273,7 +272,7 @@ class ActionsFragment : DaggerFragment() {
         binding.historyBrowser.visibility = (profile != null).toVisibility()
         binding.fill.visibility = (pump.pumpDescription.isRefillingCapable && pump.isInitialized() && !pump.isSuspended()).toVisibility()
         binding.pumpBatteryChange.visibility = (pump.pumpDescription.isBatteryReplaceable || pump.isBatteryChangeLoggingEnabled()).toVisibility()
-        binding.tempTarget.visibility = (profile != null && !loop.isDisconnected).toVisibility()
+        binding.tempTarget.visibility = (profile != null && !loop.runningMode.isLoopRunning()).toVisibility()
         binding.tddStats.visibility = pump.pumpDescription.supportsTDDs.toVisibility()
         val isPatchPump = pump.pumpDescription.isPatchPump
         binding.status.apply {

commit 4833d34e21937327352793739d43732c049ccb6c

Fix so that TT-button in Actions show when loop is running.
Typo in 2e85d3be caused this.

plugins/main/src/main/kotlin/app/aaps/plugins/main/general/actions/ActionsFragment.kt
@@ -272,7 +272,7 @@ class ActionsFragment : DaggerFragment() {
         binding.historyBrowser.visibility = (profile != null).toVisibility()
         binding.fill.visibility = (pump.pumpDescription.isRefillingCapable && pump.isInitialized() && !loop.runningMode.isSuspended()).toVisibility()
         binding.pumpBatteryChange.visibility = (pump.pumpDescription.isBatteryReplaceable || pump.isBatteryChangeLoggingEnabled()).toVisibility()
-        binding.tempTarget.visibility = (profile != null && !loop.runningMode.isLoopRunning()).toVisibility()
+        binding.tempTarget.visibility = (profile != null && loop.runningMode.isLoopRunning()).toVisibility()
         binding.tddStats.visibility = pump.pumpDescription.supportsTDDs.toVisibility()
         val isPatchPump = pump.pumpDescription.isPatchPump
         binding.status.apply {