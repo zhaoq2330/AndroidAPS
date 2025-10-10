360afed638474a1391bfb8c0d25ee738eb74f7a4
Milos Kozak <m.kozak@sysop.cz>
2025-10-10 16:52:30 +0200

    libs update

--- pump/virtual/src/main/kotlin/app/aaps/pump/virtual/composeui/TestActivity.kt
+++ pump/virtual/src/main/kotlin/app/aaps/pump/virtual/composeui/TestActivity.kt
@@ -7,8 +7,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Create
 import androidx.compose.material3.BottomSheetScaffold
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
@@ -22,7 +20,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import app.aaps.core.interfaces.logging.AAPSLogger
