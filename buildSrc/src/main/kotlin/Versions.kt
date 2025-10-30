eef6d18650e007f323059efdab5575c1cb0e5dc2
Milos Kozak <m.kozak@sysop.cz>
2025-10-30 19:55:38 +0100

    3.3.3.0-dev-d

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 @Suppress("ConstPropertyName")
 object Versions {
 
-    const val appVersion = "3.3.3.0-dev-c"
+    const val appVersion = "3.3.3.0-dev-d"
     const val versionCode = 1500
 
     const val compileSdk = 36
